112b276f4a91393dee8cbcf5df2863b659a71744
Udomomo <udomomo@gmail.com>
2023-02-11 17:15:47 +0900

    initial commit

---
+++ server/src/main/kotlin/com/udomomo/helpdesk/DddHelpdeskApplication.kt
@@ -0,0 +1,11 @@
+package com.udomomo.helpdesk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DddHelpdeskApplication
+
+fun main(args: Array<String>) {
+	runApplication<DddHelpdeskApplication>(*args)
+}
---
+++ server/src/test/kotlin/com/udomomo/helpdesk/DddHelpdeskApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.udomomo.helpdesk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DddHelpdeskApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
